794fe289bd3c7f0ba95a4784626e60a6adc299af
richmondmwangi1 <email.com>
2025-12-06 09:59:03 +0300

    updated server.js

--- server.js
+++ server.js
@@ -36,5 +36,9 @@ app.use('/image', image);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is listening at http:
+app.listen(PORT,() =>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
+});
+
+
+module.exports = app;
